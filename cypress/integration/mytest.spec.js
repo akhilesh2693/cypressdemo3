34fc8292b5bcaf20fb2a0373ef4ecc38cb937f43
akhilesh2693 <kumarakhilesh26031993@gmail.com>
2019-09-23 13:09:44 +0530

    my third commit

--- cypress/integration/mytest.spec.js
+++ cypress/integration/mytest.spec.js
@@ -1,5 +1,5 @@
 describe('Local Storage', () => {
     it( 'cypress3',() => {
-      cy.visit('https://example.cypress.io/commands/local-storage')
+      cy.visit('https://www.google.com/search?q=git+config+check&oq=git+config+check&aqs=chrome..69i57j0l5.6846j0j7&sourceid=chrome&ie=UTF-8')
     })
     })
